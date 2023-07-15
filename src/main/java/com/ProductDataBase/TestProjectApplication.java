0b7b414e185b9dc5465d7f6687c0f4848b23e361
HP <HP@192.168.0.104>
2023-07-15 16:35:49 +0500

    Some changes

--- src/main/java/com/ProductDataBase/TestProjectApplication.java
+++ src/main/java/com/ProductDataBase/TestProjectApplication.java
@@ -11,19 +11,40 @@ import com.ProductDataBase.Repository.ProductRepository;
 @SpringBootApplication
 public class TestProjectApplication {
 
+	private static final String List = null;
+
 	public static void main(String[] args) {
 	    ApplicationContext context	= SpringApplication.run(TestProjectApplication.class, args);
 	    
 	   ProductRepository ProductRepository = context.getBean(ProductRepository.class);
 	   
-	   ProductEntity product = new ProductEntity();
-	   product.setProductName("Book");
-	   product.setDetail("Chemistry");
-	   product.setPrices(1500);
+//	   ProductEntity product = new ProductEntity();
+//	   product.setProductName("Book");
+//	   product.setDetail("Chemistry");
+//	   product.setPrices(1500);
 	   
-	   ProductEntity product1 = ProductRepository.save(product);
-	   System.out.println(product1);
+	   ProductEntity product1 = new ProductEntity();
+	   product1.setProductName("Book");
+	   product1.setDetail("Islamiyat");
+	   product1.setPrices(1000);
 	   
+	   ProductEntity product2 = new ProductEntity();
+	   product2.setProductName("Book");
+	   product2.setDetail("Sindhi");
+	   product2.setPrices(1000);
+//	   Save only one Value--------------
+//	   ProductEntity product3 = ProductRepository.save(product2);
+//	   System.out.println("Product saved "+product3);
+// TODO Auto-generated method stub
+//     Use SaveAll Values---------------
+	   
+//	   java.util.List<ProductEntity> product5 = java.util.List.of(product1, product2);
+//	   
+//	   Iterable<ProductEntity> product6 = ProductRepository.saveAll(product5);
+//	   product6.forEach(users->{
+//		   System.out.println(users);
+//	   });
 	}
 
+	
 }
